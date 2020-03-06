857a2c756e7a4b8d007dd3e250bcf00e2a2f0f55
Dillon McGuire <dillon.mcguire@coatesgroup.com>
2020-03-06 17:33:16 -0600

    last commits before launch

--- src/components/SubmissionWeeks/index.js
+++ src/components/SubmissionWeeks/index.js
@@ -5,15 +5,11 @@ import SubmissionWeekPositions from "Components/SubmissionWeekPositions";
 
 export default function SubmissionWeeks({ submission, isShown }) {
   const weekKeys = Object.keys(submission).sort((a, b) => {
-    console.log("a", parseInt(a.replace("top", "").replace("finale", "-100")));
-    console.log("b", parseInt(b.replace("top", "").replace("finale", "-100")));
-
     return (
       parseInt(b.replace("top", "").replace("finale", "-100")) -
       parseInt(a.replace("top", "").replace("finale", "-100"))
     );
   });
-  console.log("weekKeys", weekKeys);
   return (
     <ul className={`submission-weeks ${isShown ? "active" : "inactive"}`}>
       {weekKeys.reverse().map((week, index) => (
