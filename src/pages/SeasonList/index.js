7fa67b7010fb1a2afc40df6c80f926236ff3951f
Dillon McGuire <dillon.mcguire@coatesgroup.com>
2021-01-12 17:05:26 -0600

    sorting and footer

--- src/pages/SeasonList/index.js
+++ src/pages/SeasonList/index.js
@@ -9,19 +9,28 @@ export default function SeasonList({ seasons }) {
     <>
       <ul className="seasons-list">
         {Object.keys(seasons) &&
-          Object.keys(seasons).map((seasonId) => (
-            <li
-              key={`season_${seasonId}`}
-              style={{ border: `2px solid ${seasons[seasonId].primary}` }}
-            >
-              <Link
-                style={{ color: `${seasons[seasonId].primary}` }}
-                to={`seasons/${seasonId}`}
+          Object.keys(seasons)
+            .sort((a, b) => seasons[a].finished - seasons[b].finished)
+            .map((seasonId) => (
+              <li
+                key={`season_${seasonId}`}
+                style={{
+                  border: seasons[seasonId].finished
+                    ? `1px solid ${seasons[seasonId].primary}`
+                    : `4px solid ${seasons[seasonId].primary}`,
+                }}
               >
-                {seasons[seasonId].name}
-              </Link>
-            </li>
-          ))}
+                <Link
+                  style={{
+                    color: `${seasons[seasonId].primary}`,
+                    fontWeight: seasons[seasonId].finished ? "regular" : "bold",
+                  }}
+                  to={`seasons/${seasonId}`}
+                >
+                  {seasons[seasonId].name}
+                </Link>
+              </li>
+            ))}
       </ul>
     </>
   );
