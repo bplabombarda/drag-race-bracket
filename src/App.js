949e03961256c5a0e0423d8cdfb757d88f5e8944
Dillon McGuire <dillonmcguire@Dillons-Air.askey.com>
2022-12-01 10:47:57 -0600

    redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Router, Link } from "@reach/router";
+import { Router, Link, Redirect } from "@reach/router";
 import Season from "Pages/Season";
 import HomePage from "Pages/HomePage";
 import firebase from "Utils/firebase";
@@ -38,6 +38,7 @@ export default function App() {
         </Link>
       </header>
       <Router>
+        <Redirect from="/" to="MMwD"/>
         <HomePage path="/" seasons={seasons} />
         <Season path="/seasons/:seasonId/*" seasons={seasons} />
         <MMWD path="/MMwD"></MMWD>
