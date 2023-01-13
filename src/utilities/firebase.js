db5fdc03593709b7fd7519451c61606f3f68530b
Dillon McGuire <dillonmcguire@Dillons-Air.askey.com>
2023-01-13 14:26:36 -0600

    remove analytics

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/analytics';
+// import 'firebase/analytics';
 
 // These environment variables are replaced by
 // Webpack's DefinePlugin as time of build.
@@ -13,6 +13,6 @@ const app = firebase.initializeApp({
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 });
 
-firebase.analytics(app);
+// firebase.analytics(app);
 
 export default app;
